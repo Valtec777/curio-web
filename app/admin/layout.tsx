05af76d9459d47af2f8f3706a9b220f8f848fce8
leleca7 <wanelle52@gmail.com>
2026-08-10 16:29:40 -0300

    Load admin generator styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,6 +4,7 @@ import { requireRole } from "@/lib/auth";
 import "./dark-mode-fixes.css";
 import "./admin-polish.css";
 import "./admin-lists.css";
+import "./admin-generator.css";
 
 export const metadata: Metadata = { robots: { index: false, follow: false } };
 
