938b369502a9472ef4e849ba64070c505d2cfc87
leleca7 <wanelle52@gmail.com>
2026-08-18 13:00:42 -0300

    Centralize character asset upload constants

---
+++ lib/character-assets.ts
@@ -0,0 +1,3 @@
+export const CHARACTER_ASSET_BUCKET = "character-assets";
+export const CHARACTER_ASSET_MAX_BYTES = 5 * 1024 * 1024;
+export const CHARACTER_ASSET_MIME_TYPES = ["image/png", "image/jpeg", "image/webp"] as const;
